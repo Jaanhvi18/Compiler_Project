a3b4a72fec6e7c391d6488217e5d75658f68c09d
“Jaanhvi <jagarwal@colgate.edu>
2024-07-06 19:53:25 -0400

    all errors + warnings resolved

--- crafting_interpreters_cpp/tree-walk interpreter/Scanner.cpp
+++ crafting_interpreters_cpp/tree-walk interpreter/Scanner.cpp
@@ -1,9 +1,8 @@
 #include "Scanner.h"
 #include "TokenType.h"
 #include "Token.h"
-#include <cctype>  // For isdigit
+#include <cctype> // For isdigit
 #include <unordered_map>
-
 
 std::unordered_map<std::string, int> keywords = {
     {"and", AND},
@@ -21,126 +20,154 @@ std::unordered_map<std::string, int> keywords = {
     {"this", THIS},
     {"true", TRUE},
     {"var", VAR},
-    {"while", WHILE}
-};
+    {"while", WHILE}};
 
-std::vector<Token> Scanner::scanTokens() {
-    //process the file
-    while (!atEnd()) {
+std::vector<Token> Scanner::scanTokens()
+{
+    // process the file
+    while (!atEnd())
+    {
         start = current;
         scanToken();
     }
 
-    //eof token simplifies later process
-    tokens.push_back(Token(EOF,"",0,1));
+    // eof token simplifies later process
+    tokens.push_back(Token(EOF, "", 0, 1));
     return tokens;
 }
 
-bool Scanner::atEnd() {
+bool Scanner::atEnd()
+{
     return current >= source.length();
 }
 
-
-
-void Scanner::scanToken() {
+void Scanner::scanToken()
+{
     // Checks and consumes the current token
     char cur = advance();
 
-    switch (cur) {
-        // The basic math operators: *, /, +, and -
-        case '(': addToken(LEFT_PAREN); break;
-        case ')': addToken(RIGHT_PAREN); break;
-        case '{': addToken(LEFT_BRACE); break;
-        case '}': addToken(RIGHT_BRACE); break;
-        case ',': addToken(COMMA); break;
-        case '.': addToken(DOT); break;
-        case '-': addToken(MINUS); break;
-        case '+': addToken(PLUS); break;
-        case ';': addToken(SEMICOLON); break;
-        case '*': addToken(STAR); break; 
-        case '%': addToken(MODULO); break;
-        case ' ':
-        case '\r':
-        case '\t':
-            // ignoring whitespace
-            break;
-        case '\n':
-            line++;
-            break;
-       default:
-            if (isdigit(cur)) {
-                int value = scanInt(cur);
-                // Aaccounting for an adding the int literal token here
-                addToken(NUMBER, value);
-            } else {
-                // Handle other cases, such as identifiers--> still need to do this
-                printf("Unrecognized character %c on line %d\n", cur, line);
-                exit(1);
-            }
-            break;
+    switch (cur)
+    {
+    // The basic math operators: *, /, +, and -
+    case '(':
+        addToken(LEFT_PAREN);
+        break;
+    case ')':
+        addToken(RIGHT_PAREN);
+        break;
+    case '{':
+        addToken(LEFT_BRACE);
+        break;
+    case '}':
+        addToken(RIGHT_BRACE);
+        break;
+    case ',':
+        addToken(COMMA);
+        break;
+    case '.':
+        addToken(DOT);
+        break;
+    case '-':
+        addToken(MINUS);
+        break;
+    case '+':
+        addToken(PLUS);
+        break;
+    case ';':
+        addToken(SEMICOLON);
+        break;
+    case '*':
+        addToken(STAR);
+        break;
+    case '%':
+        addToken(MODULO);
+        break;
+    case ' ':
+    case '\r':
+    case '\t':
+        // ignoring whitespace
+        break;
+    case '\n':
+        line++;
+        break;
+    default:
+        if (isdigit(cur))
+        {
+            int value = scanInt(cur);
+            // Aaccounting for an adding the int literal token here
+            addToken(NUMBER, value);
+        }
+        else
+        {
+            // Handle other cases, such as identifiers--> still need to do this
+            printf("Unrecognized character %c on line %d\n", cur, line);
+            exit(1);
+        }
+        break;
     }
 }
 
-
-
-char Scanner::advance() {
+char Scanner::advance()
+{
     // we need to be able to baccktrack chars if we read more than we shoudl
-    if (putback != 0){
-        char temp = putback; //checking if there is a putback char
-        putback = 0; //using the putback char
+    if (putback != 0)
+    {
+        char temp = putback; // checking if there is a putback char
+        putback = 0;         // using the putback char
         return temp;
     }
-    char c = source.at(current++); //read the next char from the source
-    if (c == '\n'){
-        line ++; //increment line count on newline
+    char c = source.at(current++); // read the next char from the source
+    if (c == '\n')
+    {
+        line++; // increment line count on newline
     }
     return c;
 }
 
-void Scanner::putBack(char c) {
-    putback = c;  // Store the character in putback
+void Scanner::putBack(char c)
+{
+    putback = c; // Store the character in putback
 }
 
-
-void Scanner::addToken(int type) {
-    addToken(type,0);
+void Scanner::addToken(int type)
+{
+    addToken(type, 0);
 }
 
-
-void Scanner::addToken(int type, int literal) {
+void Scanner::addToken(int type, int literal)
+{
     std::string text = source.substr(start, current - start);
-    tokens.push_back(Token(type,text.c_str(),literal,line));
+    tokens.push_back(Token(type, text.c_str(), literal, line));
 }
 
 // Scan and return an integer literal
 // value from the input file.
-int Scanner::scanInt(char c) {
+int Scanner::scanInt(char c)
+{
     int val = 0;
     int k;
 
     // Convert each character into an int value
-    while ((k = chrpos("0123456789", c)) >= 0) {
+    while ((k = chrpos("0123456789", c)) >= 0)
+    {
         val = val * 10 + k;
         c = advance();
     }
 
-
-  // We hit a non-integer character, put it back.
+    // We hit a non-integer character, put it back.
     putBack(c);
     return val;
 }
 
 // Return the position of character c
-int Scanner::chrpos(const std::string& s, char c) {
-    //using auto instead of std::string::size_type
-    auto pos = s.find(c);  //find is looking for the first occurence of the char in string s
+int Scanner::chrpos(const std::string &s, char c)
+{
+    // using auto instead of std::string::size_type
+    auto pos = s.find(c); // find is looking for the first occurence of the char in string s
     // also dont think we want to define an eexplicit
-    if (pos == std::string::npos) { 
-        return -1; //c was not found in s
+    if (pos == std::string::npos)
+    {
+        return -1; // c was not found in s
     }
-    return static_cast<int>(pos); //returns the position of the first occurence of the char
+    return static_cast<int>(pos); // returns the position of the first occurence of the char
 }
-
-
-
-
--- crafting_interpreters_cpp/tree-walk interpreter/Scanner.h
+++ crafting_interpreters_cpp/tree-walk interpreter/Scanner.h
@@ -4,36 +4,36 @@
 #include <unordered_map>
 // #include "Keywords.h"
 
-class Scanner {
+class Scanner
+{
 
 public:
-    Scanner(std::string src) : source(src) {};
-    ~Scanner() {};
+    Scanner(std::string src) : source(src){};
+    ~Scanner(){};
 
     std::vector<Token> scanTokens();
 
 private:
-
     std::string source;
     std::vector<Token> tokens;
-    std::unordered_map<const char*, int> keywords = {
-        {"and",AND},
-        {"and",AND},
-        {"and",AND},
-        {"and",AND},
-        {"and",AND},
-        {"and",AND},
-        {"and",AND},
-        {"and",AND},
-        {"and",AND},
-        {"and",AND},
-    };
+    // std::unordered_map<const char*, int> keywords = {
+    //     {"and",AND},
+    //     {"and",AND},
+    //     {"and",AND},
+    //     {"and",AND},
+    //     {"and",AND},
+    //     {"and",AND},
+    //     {"and",AND},
+    //     {"and",AND},
+    //     {"and",AND},
+    //     {"and",AND},
+    // };
 
     int start = 0;
     int current = 0;
     int line = 1;
 
-    char putback = 0;  // Variable to hold the putback character
+    char putback = 0; // Variable to hold the putback character
 
     bool atEnd();
 
@@ -41,14 +41,14 @@ private:
 
     char advance();
     void putBack(char);
-    
-    void addToken(int); //takes in TokenType enum value
-    void addToken(int,int); //token type and "object"
+
+    void addToken(int);      // takes in TokenType enum value
+    void addToken(int, int); // token type and "object"
 
     bool match(char);
     char peek();
     char peekNext();
     int scanInt(char);
-    int chrpos(const std::string&, char);
-    int lookupKeyword(const std::string& text);
+    int chrpos(const std::string &, char);
+    int lookupKeyword(const std::string &text);
 };
